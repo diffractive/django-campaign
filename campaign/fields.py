839d0c46c7e0c6b0e998885798428f616f59ba65
Franco de Vita <frankyhung93@gmail.com>
2020-04-29 18:05:00 +0800

    switched to external six library

--- campaign/fields.py
+++ campaign/fields.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 
+import six
 from django.conf import settings
 from django.db import models
-from django.utils import six
 from django import forms
 try:
     from django.utils import simplejson
@@ -16,17 +16,20 @@ class JSONWidget(forms.Textarea):
             value = simplejson.dumps(value, indent=2)
         return super(JSONWidget, self).render(name, value, attrs)
 
+
 class JSONFormField(forms.CharField):
     def __init__(self, *args, **kwargs):
         kwargs['widget'] = JSONWidget
         super(JSONFormField, self).__init__(*args, **kwargs)
 
     def clean(self, value):
-        if not value: return
+        if not value:
+            return
         try:
             return simplejson.loads(value)
         except Exception as exc:
             raise forms.ValidationError('JSON decode error: %s' % (str(exc),))
+
 
 class JSONField(models.TextField):
     def formfield(self, **kwargs):
@@ -43,12 +46,14 @@ class JSONField(models.TextField):
         return value
 
     def get_db_prep_save(self, value, connection=None):
-        if value is None: return
+        if value is None:
+            return
         return simplejson.dumps(value)
 
     def value_to_string(self, obj):
         value = self._get_val_from_obj(obj)
         return self.get_db_prep_value(value)
+
 
 if 'south' in settings.INSTALLED_APPS:
     from south.modelsinspector import add_introspection_rules
